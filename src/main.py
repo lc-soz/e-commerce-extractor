4093e412ca2db055393fe9e84fd92560e9ea159a
lc-soz <lclpsoz@gmail.com>
2017-11-26 15:37:44 -0300

    Main reorganized

--- src/main.py
+++ src/main.py
@@ -1,21 +1,9 @@
-import extractInfo
-import getText
-import csvManip
+import extractInfo, getText, csvManip
 
 path = "/home/lclpsoz/Dropbox/Pessoal/KABUM"
 print ("HTML TO TEXT")
-matrix = getText.getText( path + "/html/17-11-25-1906" )
+matrix = getText.getText( path + "/html/17-11-24-0557" )
 print ("TEXT TO MATRIX")
 matrix_info = extractInfo.extractInfo( matrix )
 print ("INTERFACE")
-csvManip.csvManip ( matrix_info, path , "17-11-25-1906")
-
-k = 0;
-f = open('/home/lclpsoz/Dropbox/Pessoal/KABUM/out-24.txt', 'w')
-f.write("")
-f.close
-f = open('/home/lclpsoz/Dropbox/Pessoal/KABUM/out-24.txt', 'a')
-while (matrix_info[k][0] != ""):
-    f.write(matrix_info[k][0].encode('utf-8') + "|" + matrix_info[k][1].encode('utf-8') + "|" + matrix_info[k][2].encode('utf-8') + '\n')
-    k+=1
-f.close
+csvManip.csvManip ( matrix_info, path , "17-11-24-0557")
