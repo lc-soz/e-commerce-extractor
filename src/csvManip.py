1da3a5a69d9ceb59444b10b42c5d97a253d0dbc8
lc-soz <lclpsoz@gmail.com>
2017-11-25 23:33:20 -0300

    Before changing extractInfo logic

--- src/csvManip.py
+++ src/csvManip.py
@@ -1,5 +1,7 @@
 from os import listdir
 import unicodecsv as csv
+import numpy
+import sys
 
 """
 Function receives an matrix of strings, the
@@ -13,7 +15,32 @@ def csvManip ( matrix_info, path ):
     for file_name in listdir( path ):
         if (file_name == "data.csv"):
             already_exists = True;
+
     if (not(already_exists)):
         with open(path + "/data.csv", "w") as f:
             writer = csv.writer(f)
             writer.writerows(matrix_info)
+    else:
+        with open(path + "/data.csv", "r") as f:
+            reader = csv.reader(f, delimiter=',')
+            matrix_csv = list(reader)
+
+    k = 0
+    dif = 0
+    real_dif = 0
+    for (i, j) in zip(matrix_csv, matrix_info):
+        k += 1
+        #sys.stdout.write('.')
+        if (i[1] != "" and i[1] != j[1]):
+            dif+=1
+            is_present = False
+            for l in matrix_csv:
+                if (l[1] == j[1]):
+                    is_present = True
+            if (not(is_present)):
+                real_dif+=1
+                print
+                print (k)
+                print ("\t" + i[1])
+                print ("\t" + j[1])
+    print str(dif) + " " + str(real_dif)
--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@ import getText
 import csvManip
 
 path = "/home/lclpsoz/Dropbox/Pessoal/KABUM"
-print "HTML TO TEXT"
+print ("HTML TO TEXT")
 matrix = getText.getText( path + "/html/17-11-24-0557" )
-print "TEXT TO MATRIX"
+print ("TEXT TO MATRIX")
 matrix_info = extractInfo.extractInfo( matrix )
-print "INTERFACE"
+print ("INTERFACE")
 csvManip.csvManip ( matrix_info, path )
 
 k = 0;
