29183c8c3f311c3abab5e7867b79578d92088d51
lc-soz <lclpsoz@gmail.com>
2017-11-27 00:52:43 -0300

    Break code into some new functions

--- src/csvManip.py
+++ src/csvManip.py
@@ -8,7 +8,7 @@ the file to the backup_path with a time
 in seconds concateneted to make it unique.
 """
 def backup_data ( file_path, backup_path):
-    shutil.copyfile(file_path + "/data.csv", backup_path + "/data-bckp-" + str(int(time.time())))
+    shutil.copyfile(file_path + "/data.csv", backup_path + "/data-bckp-" + str(int(time.time())) + ".csv")
 
 """
 Function receives a matrix, a path and a
@@ -20,6 +20,54 @@ def save_matrix ( matrix_to_save, path, file_name ):
     with open(path + "/" + file_name + ".csv", "w") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerows(matrix_to_save)
+
+"""
+Function receives two matrix and a value
+new_col. Returns both of them with the
+first one with the values of the second
+one in the new_col column if the second
+column is equal in that row. It also
+deletes used rows from matrix_info.
+"""
+def match_same_line ( matrix_csv, matrix_info, new_col ):
+    i = 1
+    while (i < len(matrix_csv) and i < len(matrix_info) and matrix_csv[i][0] != "" and matrix_info[i][0] != ""):
+        if (matrix_csv[i][1] == matrix_info[i][1]):
+            matrix_csv[i][new_col] = matrix_info[i][2]
+            matrix_info[i][0] = "D"
+        i+=1
+    i = 1
+    while (i < len(matrix_info)):
+        if (matrix_info[i][0] == "D" or matrix_info[i][0] == ""):
+            del (matrix_info[i])
+        else:
+            i+=1
+    return matrix_csv, matrix_info
+
+"""
+Function receives two matrix and a value
+new_col. Returns both of them with the
+first one with the values of the second
+one in the new_col column if the second
+column is equal in some row, will search
+the value of the second matrix in the first.
+"""
+def match_dif_line ( matrix_csv, matrix_info, new_col ):
+    i = 1
+    while (i < len(matrix_csv) and i < len(matrix_info) and matrix_info[i][0] != ""):
+        for row_csv in matrix_csv:
+            if (row_csv[1] == matrix_info[i][1]):
+                row_csv[new_col] = matrix_info[i][2]
+                matrix_info[i][0] = "D"
+                break
+        i+=1
+    i = 1
+    while (i < len(matrix_info)):
+        if (matrix_info[i][0] == "D" or matrix_info[i][0] == ""):
+            del (matrix_info[i])
+        else:
+            i+=1
+    return matrix_csv, matrix_info
 
 """
 Function receives a matrix of strings, the
@@ -69,83 +117,73 @@ def csvManip ( matrix_info, path, header ):
 
             k = 0
             real_dif = 0
+            matrix_csv, matrix_info = match_same_line(matrix_csv, matrix_info, new_col)
+            matrix_csv, matrix_info = match_dif_line(matrix_csv, matrix_info, new_col)
+            print matrix_info
             for (i, j) in zip(matrix_csv, matrix_info):
                 k += 1
-                #sys.stdout.write('.')
-                if (i[1] != "" and i[1] == j[1]):
-                    i[new_col] = j[2]
-                if (i[1] != ""):
-                    is_present = False
-                    for l in matrix_csv:
-                        if (l[1] == j[1]):
-                            is_present = True
-                            l[new_col] = j[2]
-                    if (not(is_present)):
-                        real_dif+=1
-                        print
-                        print k
-                        if (i[new_col-1] == j[2]):
-                            print "PRICES ARE EQUAL!"
-                        else:
-                            print "PRICES ARE DIFFERENT!"
-                        l = 0;
-                        while (i[1][l] == j[1][l]):
-                            l += 1
-                        print ("OLD: \t" + i[1] + "\t" + i[new_col-1])
-                        print ("NEW: \t" + j[1] + "\t" + j[2])
-                        print " "*(l+8) + "^"
-                        if (i[2] == j[2]):
-                            answer = "y"
-                        elif (cycles > 1):
-                            answer = raw_input("Substitute name? (y/n) ")
-                        else:
-                            answer = "n"
-                        if (answer == "y"):
-                            i[1] = j[1]
-                        elif (cycles > 1):
-                            for x in prods_inc:
-                                if (x[0] == j[0]):
-                                    if (x[0] == "GPU"):
-                                        if (x[2][19:30] == j[1][19:30]):
-                                            print x[0] + "\t" + x[1] + "\t" + x[2][19:] + "\t" + x[3]
-                                    else:
-                                        print x[0] + "\t" + x[1] + "\t" + x[2] + "\t" + x[3]
-                            are_sure = "n"
-                            answer = raw_input("There's a known equal product? (y/n) ")
-                            while (answer == 'y' and are_sure == "n"):
-                                subs_row = input("Product row:")
-                                l = 0
-                                while (matrix_csv[subs_row][1][l] == j[1][l]):
-                                    l += 1
-                                print ("OLD: \t" + matrix_csv[subs_row][1] + "\t" + matrix_csv[subs_row][2])
-                                print ("NEW: \t" + j[1] + "\t" + j[2])
-                                print " "*(l+8) + "^"
-                                are_sure = raw_input("Are you sure? (y/n)")
-                                if (are_sure == "y"):
-                                    matrix_csv[subs_row][1] = j[1]
-                                    matrix_csv[subs_row][new_col] = j[2]
-                                    x = 0;
-                                    while (x < len(prods_inc)):
-                                        if (prods_inc[x][1] == subs_row):
-                                            del(pro_inc[x])
-                                        x+=1
+                if (i[1] == "" or j[1] == ""):
+                    break
+                elif (i[1] != "TITLE"):
+                    real_dif+=1
+                    print
+                    print k
+                    if (i[new_col-1] == j[2]):
+                        print "PRICES ARE EQUAL!"
+                    else:
+                        print "PRICES ARE DIFFERENT!"
+                    l = 0;
+                    while (l < len (i[1]) and l < len(j[1]) and i[1][l] == j[1][l]):
+                        l += 1
+                    print ("OLD: \t" + i[1] + "\t" + i[new_col-1])
+                    print ("NEW: \t" + j[1] + "\t" + j[2])
+                    print " "*(l+8) + "^"
+                    if (i[new_col-1] == j[2]):
+                        answer = "y"
+                    elif (cycles > 1):
+                        answer = raw_input("Substitute name? (y/n) ")
+                    else:
+                        answer = "n"
+                    if (answer == "y"):
+                        i[1] = j[1]
+                    elif (cycles > 1):
+                        for x in prods_inc:
+                            if (x[0] == j[0]):
+                                if (x[0] == "GPU"):
+                                    if (x[2][19:30] == j[1][19:30]):
+                                        print x[0] + "\t" + x[1] + "\t" + x[2][19:] + "\t" + x[3]
                                 else:
-                                    answer = raw_input("There's a known equal product? (y/n) ")
-                            if (answer == 'n'):
-                                answer = raw_input("Add as a new product? (y/n) ")
-                                if (answer == 'y'):
-                                    matrix_csv[last_row][0] = j[0]
-                                    matrix_csv[last_row][1] = j[1]
-                                    matrix_csv[last_row][3] = j[2]
-                                    last_row+=1
+                                    print x[0] + "\t" + x[1] + "\t" + x[2] + "\t" + x[3]
+                        are_sure = "n"
+                        answer = raw_input("There's a known equal product? (y/n) ")
+                        while (answer == 'y' and are_sure == "n"):
+                            subs_row = input("Product row:")
+                            l = 0
+                            while (matrix_csv[subs_row][1][l] == j[1][l]):
+                                l += 1
+                            print ("OLD: \t" + matrix_csv[subs_row][1] + "\t" + matrix_csv[subs_row][2])
+                            print ("NEW: \t" + j[1] + "\t" + j[2])
+                            print " "*(l+8) + "^"
+                            are_sure = raw_input("Are you sure? (y/n)")
+                            if (are_sure == "y"):
+                                matrix_csv[subs_row][1] = j[1]
+                                matrix_csv[subs_row][new_col] = j[2]
+                                x = 0;
+                                while (x < len(prods_inc)):
+                                    if (prods_inc[x][1] == subs_row):
+                                        del(pro_inc[x])
+                                    x+=1
+                            else:
+                                answer = raw_input("There's a known equal product? (y/n) ")
+                        if (answer == 'n'):
+                            answer = raw_input("Add as a new product? (y/n) ")
+                            if (answer == 'y'):
+                                matrix_csv[last_row][0] = j[0]
+                                matrix_csv[last_row][1] = j[1]
+                                matrix_csv[last_row][3] = j[2]
+                                last_row+=1
 
-            m = last_row
-            while (matrix_info[m][0] != ""):
-                for l in matrix_csv:
-                    if (l[1] == matrix_info[m][1]):
-                        is_present = True
-                        l[new_col] = matrix_info[m][2]
-                m+=1
+            matrix_csv, matrix_info = match_dif_line(matrix_csv, matrix_info, new_col)
 
             prods_inc = [["CAT" , "ROW", "NAME", "PRICE"]]
             x = 0
